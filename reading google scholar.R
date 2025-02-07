e4a7d629402d7df5afc1ae1eeabe239d255a2eb6
Nick McKay <nick@mckays.us>
2025-02-07 11:14:11 -0700

    tinkering

--- reading google scholar.R
+++ reading google scholar.R
@@ -1,65 +1,42 @@
-Sys.sleep(6000)
-
 library(rvest)
 library(scholar)
 library(lubridate)
+library(tidyverse)
 
 idtab=read.csv("facultygooglescholarids.csv")
 y=year(Sys.Date())
 idtab$EndYear[is.na(idtab$EndYear)==T]<-y
 
 scholdat=data.frame()
-
+#get primary data first
 for(i in 1:nrow(idtab)){
   dattemp=get_publications(idtab$ID[i])
   dattemp=dattemp[dattemp$year>=idtab$StartYear[i] & !(dattemp$year>idtab$EndYear[i]),]
   dattemp$Faculty=rep(idtab$Name[i],nrow(dattemp))
-  
-  if(nrow(dattemp) == 0){
-    next
-  }
-  
-  #get all the coauthors
-  if(nrow(dattemp) < 50){
-    allAuthors <- try(scholar::get_complete_authors(idtab$ID[i],pubid = dattemp$pubid))
-    if(!is(allAuthors,"try-error")){
-      dattemp$author <- allAuthors
-    }
-  }else{
-    nr <- nrow(dattemp)
-    firstRow <- 1
-    nextRow <- 50
-    allAuthors <- c()
-    while(TRUE){
-      newAuthors <- try(scholar::get_complete_authors(idtab$ID[i],pubid = dattemp$pubid[firstRow:nextRow]))
-      if(is(newAuthors,"try-error")){
-        break
-      }
-      allAuthors <- c(allAuthors,newAuthors)
-      
-      if(nextRow >= nr){
-        break
-      }
-      ns <- min(nr - nextRow,50)
-      
-      nextRow <- nextRow + ns
-      firstRow <- firstRow + 50
-      Sys.sleep(30)
-    }
-    
-    if(!is(newAuthors,"try-error")){
-      dattemp$author <- allAuthors
-    }
-    
-  }
-  
   scholdat=rbind(scholdat,dattemp)
-  Sys.sleep(60)
 }
+
 
 meetingnames="(eeting|bstracts|onference|Joint|abstract|symposium)"
 
 scholdat$Paper=ifelse(grepl(meetingnames, scholdat$journal),"Meeting","Paper")
+
+
+#get all publications
+allPubs <- scholdat |> 
+  filter(year > 2009, 
+         Paper == "Paper",
+         journal != "",
+         !is.na(Faculty)) 
+
+
+length(unique(allPubs$pubid))
+
+#figure out which ones need additional coauthors
+needMoreAuthors <- which(str_detect(scholdat$author,pattern = "..."))
+
+
+
 
 output=scholdat[scholdat$year>2009 & scholdat$Paper=="Paper" & scholdat$journal!="",]
 output=output[is.na(output$Faculty)==FALSE,]
